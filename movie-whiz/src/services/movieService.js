20b47356a4c78126d19a8ae5b510391b89a49fca
memesofting <bibitayoadekunle00@gmail.com>
2025-10-17 23:24:38 +0100

    fixing deployment

--- movie-whiz/src/services/movieService.js
+++ movie-whiz/src/services/movieService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 
 async function fetchMovies(query) {
     try {
