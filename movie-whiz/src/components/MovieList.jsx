76b7e663a147d51f9bcebd6252beaf2a4a0845f6
memesofting <bibitayoadekunle00@gmail.com>
2025-10-18 01:45:23 +0100

    more styling

--- movie-whiz/src/components/MovieList.jsx
+++ movie-whiz/src/components/MovieList.jsx
@@ -4,16 +4,16 @@ function MovieList({ movies }) {
     if (!movies || movies.length === 0) {
         return (
             <>
-            <h2 className="m-5 text-blue-900">Search result</h2>
-                <p className="text-center text-gray-500 mt-5">No movies found.
+            <h2 className="p-3 text-blue-900 bg-blue-400">Search result</h2>
+                <p className="text-center text-gray-500 pt-5 pb-5 bg-gray-100">No movies found.
                 </p>
             </>
         );
     }
     return (
         <>
-            <h2 className="m-5 text-blue-900">Search result</h2>
-            <ul className="flex flex-row flex-wrap justify-center mt-5">
+            <h2 className="p-3 text-blue-900 bg-blue-400">Search result</h2>
+            <ul className="flex flex-row flex-wrap justify-center pt-5 pb-5 bg-gray-100">
                 {movies.map((movie) => (
                     <Link key={movie.imdbID} to={`/movie/${movie.imdbID}`}>
                         <li key={movie.imdbID} className="p-2 w-50 hover:scale-105 transition duration-550 ease-in-out cursor-pointer">
--- movie-whiz/src/components/RandomMovies.jsx
+++ movie-whiz/src/components/RandomMovies.jsx
@@ -9,7 +9,7 @@ function RandomMovies() {
     useEffect(() => {
         async function getRandomMovies() {
             setIsLoading(true);
-            const randomKeywords = ['god', 'love', 'gold', 'me', 'lord', 'star', 'food', 'money'];
+            const randomKeywords = ['god', 'love', 'gold', 'me', 'lord', 'star', 'food', 'money', 'will'];
             const randomQuery = randomKeywords[Math.floor(Math.random() * randomKeywords.length)];
             const randomMovies = await fetchMovies(randomQuery)
             setMovies(randomMovies);
@@ -26,8 +26,8 @@ function RandomMovies() {
     }
     return (
         <>
-            <h2 className="m-5 text-blue-900">Movies</h2>
-            <ul className="flex flex-row flex-wrap justify-center mt-5">
+            <h2 className="p-3 text-blue-900 bg-blue-400">Movies</h2>
+            <ul className="flex flex-row flex-wrap justify-center pt-5 pb-5 bg-gray-100">
                 {movies.map((movie) => (
                     <Link key={movie.imdbID} to={`/movie/${movie.imdbID}`}>
                         <li key={movie.imdbID} className="p-2 w-50 hover:scale-105 transition duration-550 ease-in-out cursor-pointer">
